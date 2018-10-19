ed467ca227953621d3a178042d0d94a4d41fbdbc
sigita <cepauskiki@gmail.com>
2018-10-19 18:13:37 +0300

    mysql homework

---
+++ pratybos8.sql
@@ -0,0 +1,34 @@
+--
+-- Database: `Books`
+--
+
+
+--
+-- Table structure for table `Authors` - authors of the books
+--
+
+CREATE TABLE IF NOT EXISTS `Authors` (
+  `authorId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_german1_ci NOT NULL,
+  PRIMARY KEY (`authorId`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+
+
+
+--
+-- Table structure for table `Books` - books with only main info
+--
+
+CREATE TABLE IF NOT EXISTS `Books` (
+  `bookId` int(11) NOT NULL AUTO_INCREMENT,
+  `authorId` int(11) DEFAULT NULL,
+  `title` varchar(255) COLLATE latin1_general_ci NOT NULL,
+  `year` year(4) DEFAULT NULL,
+  PRIMARY KEY (`bookId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=9 ;
+
+
+CREATE TABLE authors_books (authorId int, bookId int);
+ALTER TABLE books DROP COLUMN authorId;
+ALTER TABLE books ADD COLUMN title1 VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_lithuanian_ci DEFAULT NULL AFTER title;
+
